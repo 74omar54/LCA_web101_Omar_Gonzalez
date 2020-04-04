78376b5af24ccb9c86ac2888e32da080af844ebb
74omar54 <omargonzalez7474@gmail.com>
2020-04-04 02:51:31 -0500

    methods

--- cohort-20201/ManyMethods/Program.cs
+++ cohort-20201/ManyMethods/Program.cs
@@ -8,25 +8,29 @@ namespace ManyMethods
         {
             Hello();
             Addition();
-            catDog();
-            oddEvent();
-            inches();
-            echo();
-            killGrams();
-            date();
-            age();
-            guess();
+            CatDog();
+            OddEvent();
+            Inches();
+            Echo();
+            KillGrams();
+            Date();
+            Age();
+            Guess();
 
 
 
 
         }
+
         public static void Hello()
         {
             Console.WriteLine("What is your name?");
             Console.ReadLine();
             Console.WriteLine("Bye Bob");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
+
         public static void Addition()
         {
             Console.WriteLine("Enter your first number");
@@ -36,9 +40,12 @@ namespace ManyMethods
 
             int first = Convert.ToInt32(answer);
             int second = Convert.ToInt32(secondAnswer);
-            Console.WriteLine("the sum is- " + (first + second));
+            Console.WriteLine("the sum is " + (first + second));
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
-        public static void catDog()
+
+        public static void CatDog()
         {
             string cats = "cats";
             string dogs = "dogs";
@@ -46,7 +53,7 @@ namespace ManyMethods
             Console.WriteLine("cats or dogs?");
 
 
-            string answer = Console.ReadLine();
+            string answer = Console.ReadLine().ToLower();
 
             if (answer == cats)
             {
@@ -56,15 +63,22 @@ namespace ManyMethods
             {
                 Console.WriteLine("BARK");
             }
+            else
+            {
+                Console.WriteLine("Must choose cats or dogs");
+            }
+            Console.WriteLine("Press any key to continue");
             Console.ReadKey();
 
 
         }
-        public static void oddEvent()
+
+        public static void OddEvent()
         {
             int i;
 
             Console.WriteLine("Enter a number!");
+
             i = int.Parse(Console.ReadLine());
             if (i % 2 == 0)
             {
@@ -76,31 +90,97 @@ namespace ManyMethods
                 Console.Write("the number you have entered is Odd");
                 Console.Read();
             }
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+            //Need to find a way to restart the process of cats and dogs if somebody enters somehting rather than cats or dogs.
+            
 
         }
-        public static void inches()
+
+        public static void Inches()
         {
+            double inch;
             Console.WriteLine("Enter a height in ft.");
+            double feet = Convert.ToDouble(Console.ReadLine());
+            inch = feet * 12;
+            Console.WriteLine("{0} Feet : {1} Inches", feet, inch);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
         }
-        public static void echo()
+
+        public static void Echo()
         {
             Console.WriteLine("Enter a word");
+            string answer = Console.ReadLine();
+
+            Console.WriteLine(answer.ToUpper());
+            Console.WriteLine(answer.ToLower());
+            Console.WriteLine(answer.ToLower());
+            Console.Read();
+
+
+       
         }
-        public static void killGrams()
+
+        public static void KillGrams()
         {
             Console.WriteLine("Enter a weight in pounds");
-        }
-        public static void date()
-        {
+            double pounds = Convert.ToDouble(Console.ReadLine());
+
+            double kilograms = pounds * 0.45359237; //This is the conversion rate of pounds to killograms.
+            Console.WriteLine(pounds +  " Pounds is "  +  kilograms  +  " killograms");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
 
         }
-        public static void age()
+
+        public static void Date()
+        {
+            Console.WriteLine("The current date and time is " + DateTime.Now);
+            Console.Read();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        public static void Age()
         {
             Console.WriteLine("What year were you born?");
+            string answer = Console.ReadLine();
+
+
+            int year = Convert.ToInt32(answer);
+            int DOB = 2020 - year;
+
+            Console.WriteLine("Your age is " + DOB);
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            
+
+           
         }
-        public static void guess()
+
+        public static void Guess()
         {
-            Console.WriteLine("Guess a programming language?");
+            string csharp = "csharp";
+
+            Console.WriteLine("I am thinking of a programming language, what is it?");
+
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == csharp)
+            {
+                Console.WriteLine("CORRECT");
+            } else
+            {
+                Console.WriteLine("WRONG");
+            }
+
+            Console.WriteLine("You made it all the way to the bottom without breaking me CONGRATS!!!");
+           
+         
+
         }
 
     }
