7f7f4e559778bcbdc99349ece90c68adb159c87a
74omar54 <omargonzalez7474@gmail.com>
2020-06-17 17:10:25 -0500

    ?

---
+++ cohort-20201/ToDoItem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ToDoItem
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            string answer;
+            string todoItem;
+            Console.WriteLine("Would you like to add an item? Yes or No ");
+            answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("what is the description, due date, and priority of the item?");
+                todoItem = Console.ReadLine();
+
+
+            }
+            else
+            {
+                Console.WriteLine("goodbye");
+            }
+
+        }
+        public class Todo
+        {
+            string Description;
+            int DueDate;
+            string Priority;
+
+            public 
+            
+        }
+    }
+}
